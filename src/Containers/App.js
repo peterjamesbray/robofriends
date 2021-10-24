fc1fc0ee2e58a20bad1f44c535c0a20f6fadbd9f
Gameishard <peterbray@internode.on.net>
2021-10-24 14:26:57 +1030

    uploaded robofriends

---
+++ src/Containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Cardlist from '../Components/Cardlist';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll'
+import './App.css';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({robots: users}));
+        
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })  
+    }
+
+    render () {
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else{
+        return(
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <Cardlist robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+        }
+    }
+    
+}
+
+export default App;
